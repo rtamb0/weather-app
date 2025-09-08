3e8c59e15af4d41b3e8a2ba3af3ed125dfac9a98
Ralf <135024553+rtamb0@users.noreply.github.com>
2025-09-08 14:26:15 +0700

    Add call API function

---
+++ src/api.js
@@ -0,0 +1,26 @@
+export const getWeatherData = async (location) => {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=GYF63P52G9MSB9RPDQJGURN3H`
+    );
+    if (!response.ok) {
+      const error = await response.text();
+      throw new Error(error);
+    }
+    const result = await response.json();
+    const processedResult = processWeatherData(result);
+    return processedResult;
+  } catch (error) {
+    return error;
+  }
+};
+
+const processWeatherData = (data) => {
+  return {
+    location: data.resolvedAddress,
+    timezone: data.timezone,
+    description: data.description,
+    currentWeather: data.currentConditions,
+    forecast: data.days,
+  };
+};
