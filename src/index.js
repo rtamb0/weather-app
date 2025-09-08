685f94bdf29ef30b79c1fa78f9bef374a61ef9f0
Ralf <135024553+rtamb0@users.noreply.github.com>
2025-09-08 14:52:07 +0700

    Allow user to input the location and show its weather info in the html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import "normalize.css";
+import { getWeatherData } from "./api";
+
+const elements = {
+  locationInput: document.querySelector("#locationInput"),
+  searchButton: document.querySelector("#searchButton"),
+  infoWrapper: document.querySelector("#displayInfo"),
+};
+
+const clearInfoWrapper = () => {
+  while (elements.infoWrapper.hasChildNodes()) {
+    const lis = elements.infoWrapper.childNodes;
+    for (const li of lis) {
+      elements.infoWrapper.removeChild(li);
+    }
+  }
+};
+
+elements.searchButton.addEventListener("click", () => {
+  const value = elements.locationInput.value;
+  if (value) {
+    getWeatherData(value)
+      .then((result) => {
+        clearInfoWrapper();
+        for (const info in result) {
+          const li = document.createElement("li");
+          li.textContent = result[info];
+          elements.infoWrapper.appendChild(li);
+        }
+      })
+      .catch((error) => {
+        clearInfoWrapper();
+        console.log(error);
+        const li = document.createElement("li");
+        li.textContent = error.message;
+        elements.infoWrapper.appendChild(li);
+      });
+  }
+});
